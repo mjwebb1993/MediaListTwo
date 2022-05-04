d84cb4b6169ef5ad0097684260894b75a963e128
webby12333 <webby12333@yahoo.com>
2022-05-03 23:05:46 -0500

    updating list schema

--- server/models/list.js
+++ server/models/list.js
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const book = require("./book");
-const game = require("./game");
-const movie = require("./movie");
+
 
 // Demonstrate linking vs embedding
 const listSchema = new mongoose.Schema({
-    movie: [movie.schema],
-    book: [book.schema],
-    game: [game.schema]
+    movie: [{ type: Schema.Types.ObjectId, ref: "Movie" }],
+    book: [{ type: Schema.Types.ObjectId, ref: "Book" }],
+    game: [{ type: Schema.Types.ObjectId, ref: "Game" }]
   });
   
   const List = mongoose.model("List", listSchema);
